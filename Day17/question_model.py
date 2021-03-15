2e759d6a6ea62795bd20df045fe454361b474080
SquallyWally <mlk_shadow@hotmail.com>
2021-03-15 01:27:59 +0100

    add Day 17

---
+++ Day17/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, text, antwoord):
+        self.antwoord = antwoord
+        self.text = text
+
+
+#new_vraag = Question("Makker", "False")
+#print(new_vraag.text)
